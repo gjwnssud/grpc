3a9a5702ba339df74a89086117686374a1e9c782
hzn <gjwnssud@gmail.com>
2023-08-08 08:04:38 +0900

    modify pom.xml and dto

--- grpc-server/src/main/java/com/hzn/grpc/server/dto/KeyPair.java
+++ grpc-server/src/main/java/com/hzn/grpc/server/dto/KeyPair.java
@@ -3,7 +3,7 @@ package com.hzn.grpc.server.dto;
 import lombok.Data;
 
 @Data
-public class NommKeyPair {
+public class KeyPair {
 	private String privateKey;
 	private String publicKey;
 	private String address;
